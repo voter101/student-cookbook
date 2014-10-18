1fbe6eff7c8a1d19bb1b830d49db31872684ed05
Wiktor Mociun <voter101@gmail.com>
2014-10-18 20:54:44 +0200

    Recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :name, :prepare_time, :recipe
+
+  validates_presence_of :name, :recipe
+  validates_numericality_of :prepare_time, greater_than: 0, message: '- should be larger than 0'
+  validates_numericality_of :prepare_time, less_than_or_equal_to: 5, message: '- more than 5 minutes is too long for a student'
 end
